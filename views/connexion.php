cd88e57dd52e15500d691a539b289f105e1c2ef0
roxylya <adelinejeuniaux@gmail.com>
2023-01-27 14:34:47 +0100

    form connexion et inscription ok

--- views/connexion.php
+++ views/connexion.php
@@ -1,34 +1,33 @@
 <main class="container-fluid">
     <div class="row">
         <div class="col">
-            <div class="bgcgu d-flex flex-column m-lg-5">
-                <div class="text-center ">
-                    <h1><span class="bdRenaissanceH1">C</span>onnexion </h1>
-                </div>
-
-                <div class="pt-lg-5 pt-3">
-                    <form method="post" class="m-3 p-3 my-lg-3 px-lg-5" enctype="multipart/form-data" novalidate>
-                        <div class="form-group">
+            <div class="bgcgu d-flex flex-column justify-content-center align-items-center m-lg-5">
+                <h1><span class="bdRenaissanceH1">C</span>onnexion </h1>
+                <form method="post" class="connect my-5" enctype="multipart/form-data" novalidate>
+                    <div class="d-lg-flex flex-wrap justify-content-around align-items-center pt-3">
+                        <div class="form-group ">
                             <label for="email">Email :</label>
                             <input type="email" class="form-control" id="email" name="email" value="" pattern="" required>
-                            <p>Message d'alerte</p>
+                            <p class="error">Message d'alerte</p>
                         </div>
 
                         <div class="form-group">
                             <label for="password">Mot de passe :</label>
                             <input type="password" class="form-control" id="password" name="password" value="" pattern="" required>
-                            <p id="alertPassword1">Message d'alerte</p>
+                            <p class="error" id="alertPassword1">Message d'alerte</p>
                         </div>
-            
-                        <div class="form-group pt-3 text-center">
-                            <button type="submit" id="submit" class="btn btn-default">Envoyer</button>
-                        </div>
-
-                       <p> Pas encore inscrit ? C'est par <a href="/controllers/formCtrl.php">ici</a>.</p>
-                    </form>
-                    <div class="text-center py-3">
-                        <a href="#"><img class="chandlierReturn" title="Retour en haut" src="/public/assets/img/chandelier1.png" alt="chandelier"></a>
                     </div>
-                </div>
+                    <div class="form-group text-center">
+                        <button type="submit" id="submit" class="btn btn-default">Envoyer</button>
+                    </div>
+                    <div class="pt-3 text-center">
+                        <p> Pas encore inscrit ? C'est par <a href="/controllers/formCtrl.php">ici</a>.</p>
+                    </div>
             </div>
+            </form>
+            <div class="text-center pt-3">
+                <a href="#"><img class="chandlierReturn" title="Retour en haut" src="/public/assets/img/chandelier1.png" alt="chandelier"></a>
+            </div>
+        </div>
+    </div>
 </main>
--- views/form.php
+++ views/form.php
@@ -1,50 +1,51 @@
 <main class="container-fluid">
     <div class="row">
         <div class="col">
-            <div class="bgcgu d-flex flex-column m-lg-5">
-                <div class="text-center ">
-                    <h1><span class="bdRenaissanceH1">I</span>nscription </h1>
-                </div>
-
-                <div class="pt-lg-5 pt-3">
-                    <form method="post" class="m-3 p-3 my-lg-3 px-lg-5" enctype="multipart/form-data" novalidate>
-                        <div class="form-group">
+            <div class="bgcgu d-flex flex-column justify-content-center align-items-center m-lg-5">
+                <h1><span class="bdRenaissanceH1">I</span>nscription </h1>
+                <form method="post" class="createAccount my-5" enctype="multipart/form-data" novalidate>
+                    <div class="d-lg-flex flex-wrap justify-content-around align-items-center pt-3">
+                        <div class="form-group col-lg-5">
                             <label for="email">Email :</label>
                             <input type="email" class="form-control" id="email" name="email" value="" pattern="" required>
-                            <p>Message d'alerte</p>
+                            <p class="error">Message d'alerte</p>
                         </div>
-
-                        <div class="form-group">
-                            <label for="password">Mot de passe :</label>
-                            <input type="password" class="form-control" id="password" name="password" value="" pattern="" required>
-                            <p id="alertPassword1">Message d'alerte</p>
-                        </div>
-                        <div class="form-group">
-                            <label for="passwordConfirm">Confirmer mot de passe :</label>
-                            <input type="password" class="form-control" id="passwordConfirm" name="passwordConfirm" value="" pattern="" required>
-                            <p id="alertPassword2">Message d'alerte</p>
-                            <p id="alertPassword3"></p>
-                        </div>
-                        <!--    REGEX PASSWORD : min 8, 1 maj, 1 min, 1 chiffre, 1 caractère spécial-->
-
-                        <div class="form-group">
+                        <div class="form-group col-lg-5 ">
                             <label for="firstname">Pseudo RR:</label>
                             <input type="text" class="form-control" id="firstname" name="firstname" value="" pattern="" required>
-                            <p>Message d'alerte</p>
+                            <p class="error">Message d'alerte</p>
                         </div>
+                    </div>
+                    <div class="d-lg-flex flex-wrap justify-content-around align-items-center pt-lg-3">
+                        <div class="form-group col-lg-5">
+                            <label for="password">Mot de passe :</label>
+                            <input type="password" class="form-control" id="password" name="password" value="" pattern="" required>
+                            <p class="error" id="alertPassword1">Message d'alerte</p>
+                        </div>
+                        <div class="form-group col-lg-5 ">
+                            <label for="passwordConfirm">Confirmer mot de passe :</label>
+                            <input type="password" class="form-control" id="passwordConfirm" name="passwordConfirm" value="" pattern="" required>
+                            <p class="error" id="alertPassword2">Message d'alerte</p>
+                        </div>
+                    </div>
 
-                        <div class="form-group">
+                    <!--    REGEX PASSWORD : min 8, 1 maj, 1 min, 1 chiffre, 1 caractère spécial-->
+                    <div class="d-lg-flex flex-wrap justify-content-around align-items-center pt-lg-2">
+                        <div class="form-group col-lg-11">
                             <label for="avatar" class="form-label">Avatar :</label>
                             <input class="form-control" type="file" id="avatar" name="avatar" accept="image/png, image/jpeg" required>
-                            <p>Message d'alerte</p>
+                            <p class="error">Message d'alerte</p>
                         </div>
-                        <div class="form-group pt-3 text-center">
-                            <button type="submit" id="submit" class="btn btn-default">Envoyer</button>
-                        </div>
-                    </form>
-                    <div class="text-center py-3">
-                        <a href="#"><img class="chandlierReturn" title="Retour en haut" src="/public/assets/img/chandelier1.png" alt="chandelier"></a>
                     </div>
+                    <div class="form-group text-center">
+                        <button type="submit" id="submit" class="btn">Envoyer</button>
+                    </div>
+
+                </form>
+                <div class="text-center">
+                    <a href="#"><img class="chandlierReturn" title="Retour en haut" src="/public/assets/img/chandelier1.png" alt="chandelier"></a>
                 </div>
             </div>
+        </div>
+    </div>
 </main>
