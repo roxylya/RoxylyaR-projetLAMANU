27ed880ea5c1c17ab205e6e419a740e61984f5bd
roxylya <adelinejeuniaux@gmail.com>
2023-01-27 10:50:27 +0100

    maj cgu

---
+++ views/cgu.php
@@ -0,0 +1,115 @@
+    <!-- main  start -->
+    <main class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <div class="bgcgu d-flex flex-column">
+                    <div class="text-center ">
+                        <h1><span class="bdRenaissanceH1">C</span>onditions <span class="bdRenaissanceH1">G</span>énérales d' <span class="bdRenaissanceH1">U</span>tilisation</h1>
+                    </div>
+                    <div class="pt-5">
+                        <h2><span class="bdRenaissance">P</span>résentation :</h2>
+                        <p>Le site Roxylya”R” est utilisé comme un atelier d’art gratuit, issu du jeu Les
+                            Royaumes
+                            Renaissants. L’utilisateur qui s’inscrit pourra commander une tenue, un avatar et/ou une bannière
+                            pour son personnage des Royaumes Renaissants. Il pourra ensuite l’ utiliser sur le forum officiel du
+                            jeu. Il n’y a pas de RP sur ce site, l’utilisateur est invité à utiliser les forums officiels du jeu
+                            Les Royaumes Renaissants prévus à cet effet.</p>
+                        <p>Cet atelier numérique est un projet d’école. Sa vocation est d’évoluer de la
+                            même manière que les
+                            compétences de la créatrice. Ainsi, si vous avez des remarques ou constatations, merci de les
+                            envoyer à l'admin via le formulaire de contact.</p>
+                    </div>
+                    <div class="pt-5">
+                        <h2><span class="bdRenaissance">P</span>lan du site :</h2>
+                        Depuis la page d'accueil, il est possible pour l'utilisateur de :
+                        <ul>
+                            <li>Se connecter ou s’inscrire</li>
+                            <li>Consulter le catalogue</li>
+                            <li>Consulter la galerie</li>
+                            <li>Consulter le livre d'or</li>
+                            <li>Accéder au jeu Les Royaumes Renaissants (Renaissance Kingdoms)</li>
+                            <li>Consulter les mentions légales</li>
+                            <li>Envoyer un mail à l’administrateur</li>
+                        </ul>
+
+                        Depuis le livre d'or :
+                        <ul>
+                            <li>Les visiteurs pourront lire les messages déposés par les inscrits</li>
+                            <li>Les utilisateurs dits inscrits pourront lire et déposer des messages.</li>
+                        </ul>
+                        Depuis le compte Utilisateur, il sera possible de :
+                        <ul>
+                            <li>Choisir un avatar</li>
+                            <li>Modifier son mot de passe</li>
+                            <li>Supprimer son compte</li>
+                            <li>Commander une tenue, un avatar et une bannière</li>
+                            <li>Importer des fichiers images</li>
+                            <li>Télécharger sa commande</li>
+                            <li>Consulter tous ses messages</li>
+                            <li>Consulter toutes ses commandes</li>
+                            <li>Consulter le catalogue</li>
+                            <li>Consulter le livre d'or</li>
+                        </ul>
+                        Depuis le compte Administrateur, il sera possible de :
+                        <ul>
+                            <li>Choisir/Modifier son avatar</li>
+                            <li>Choisir/Modifier son pseudo</li>
+                            <li>Modifier son mot de passe</li>
+                            <li>Modifier son mail</li>
+                            <li>Consulter/Supprimer les messages sur le livre d'or</li>
+                            <li>Accéder à la liste des utilisateurs(inscrits)/admin et leurs données (pseudos, mail)</li>
+                            <li>Ajouter/Supprimer un compte admin/utilisateur</li>
+                            <li>Consulter/Ajouter/Supprimer des éléments du Catalogue/de la Galerie</li>
+                            <li>Accéder à la liste de toutes les commandes réalisés et en attente</li>
+                            <li>Télécharger les fichiers images envoyés par les utilisateurs</li>
+                            <li>Importer les fichiers images et transférer vers le compte utilisateur</li>
+                        </ul>
+                    </div>
+                    <div class="pt-5">
+                        <h2><span class="bdRenaissance">C</span>ompte Utilisateur :</h2>
+                        <p>
+                            Comme indiqué à diverses reprises, pour commander et commenter, il faut être inscrit. L'Utilisateur
+                            a
+                            besoin pour cela, d'une adresse mail, de son pseudo de joueur des Royaumes Renaissants, et d'un mot
+                            de passe.
+                            Une fois que l'Utilisateur a cliqué sur le lien de confirmation reçu sur l'adresse mail enregistrée,
+                            précédemment, il pourra se connecter sur son compte utilisateur, passer une commande et commenter le
+                            livre d'or.
+                        </p>
+                        <p>
+                            Dans le cadre du Réglement Général de la Protection des Données (RGPD), l'utilisateur peut supprimer
+                            son compte les données personnelles seront effacées.
+                            Dans le cas où un/des message(s) aura/auront été posté(s), le pseudo affiché sera remplacé par
+                            invité. <br>
+                            Avant de supprimer son compte, l'Utilisateur peut également faire une demande de suppression de ses
+                            messages à l'Administrateur, via le formulaire de contact situé en bas de page du site. Une fois le
+                            compte supprimé, les messages "invité" ne seront supprimé qu'au bon vouloir de l'Administrateur,
+                            pour des raisons principalement de concordance.
+                            Une fois le compte supprimé, même l'Administrateur ne pourra savoir à qui appartient le message d'un
+                            pseudo
+                            Invité.
+                        </p>
+                    </div>
+                    <div class="pt-5">
+                        <h2><span class="bdRenaissance">M</span>odération :</h2>
+                        <p>
+                            Roxylya "R" est un site sur lequel l’Utilisateur peut laisser des messages sur le livre d’or,
+                            visible par tous; dans ses commandes destinées aux créateurs ainsi que dans contact, pour s’adresser à
+                            l’Administrateur. De ce fait, l’Utilisateur se doit de respecter chacun. Tout propos jugé indécent,
+                            irrespectueux ou nocif sera amputé voir supprimé par l’Administrateur. Si l’utilisateur récidive son
+                            compte sera désactivé. Tout échange devra donc être cordial, constructif afin que chacun poursuive sa
+                            navigation dans la sérénité.
+                        </p>
+                    </div>
+                    <div class="pt-5 text-center">
+                        <h2><span class="bdRenaissance ps-5">J</span>e vous souhaite une agréable navigation !</h2>
+                    </div>
+                    <div class="text-center pt-5">
+                        <a href="#"><img class="chandlierReturn" title="Retour en haut" src="/public/assets/img/chandelier1.png" alt="chandelier"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <!-- main end -->
