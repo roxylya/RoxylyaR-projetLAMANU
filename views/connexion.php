68b0edafde19f8c7d22923e885745f488de973c3
roxylya <adelinejeuniaux@gmail.com>
2023-01-27 11:40:40 +0100

    structure html ok

---
+++ views/connexion.php
@@ -0,0 +1,33 @@
+<main class="container-fluid">
+    <div class="row">
+        <div class="col">
+            <div class="bgcgu d-flex flex-column m-lg-5">
+                <div class="text-center ">
+                    <h1><span class="bdRenaissanceH1">C</span>onnexion </h1>
+                </div>
+
+                <div class="pt-lg-5 pt-3">
+
+                </div>
+
+                <div class="pt-lg-5 pt-3">
+                  
+                </div>
+
+                <div class="pt-lg-5 pt-3">
+                
+                </div>
+
+                <div class="pt-lg-5 pt-3">
+
+                </div>
+
+                <div class="pt-lg-5 pt-3">
+                   
+                </div>
+                <div class="text-center py-3">
+                    <a href="#"><img class="chandlierReturn" title="Retour en haut" src="/public/assets/img/chandelier1.png" alt="chandelier"></a>
+                </div>
+            </div>
+        </div>
+</main>
