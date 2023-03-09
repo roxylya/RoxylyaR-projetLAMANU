af52d9ae267f23c45647da664fce0ce9a5f33786
roxylya <adelinejeuniaux@gmail.com>
2023-03-09 15:47:31 +0100

    changement chemins

---
+++ views/userAccount/userAccount.php
@@ -0,0 +1,15 @@
+<main class="container-fluid py-5">
+    <div class="row">
+        <div class="col">
+            <div class="d-flex flex-column m-5">
+                <div class="text-center ">
+                    <h1><span class="bdRenaissanceH1">B</span>ienvenue Roxylya !</h1>
+                </div>
+                
+
+                <div class="text-center py-3">
+                    <a href="#"><img class="chandlierReturn" title="Retour en haut" src="/public/assets/img/chandelier1.png" alt="chandelier"></a>
+                </div>
+            </div>
+        </div>
+</main>
