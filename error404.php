7e83ac24ffecd2a8e7ef962ed66a43e40de5a6c7
roxylya <adelinejeuniaux@gmail.com>
2023-03-09 12:19:12 +0100

    verif

---
+++ error404.php
@@ -0,0 +1,12 @@
+<main class="container-fluid">
+    <div class="row">
+        <div class="col">
+            <div class="d-flex flex-column justify-content-center align-items-center m-lg-5">
+                <h1 class="m-5">Erreur 404</h1> 
+                <h2 class="mb-5">Page Introuvable</h2> 
+                <div class="text-center py-3">
+                    <a href="#"><img class="chandlierReturn" title="Retour en haut" src="/public/assets/img/chandelier1.png" alt="chandelier"></a>
+                </div>
+            </div>
+        </div>
+</main>
