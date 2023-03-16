f78767f5720829465106e14d9a81af18a888d3fd
roxylya <adelinejeuniaux@gmail.com>
2023-03-16 12:19:58 +0100

    suppression des images inutilisées

--- views/userAccount/userAccount.php
+++ views/userAccount/userAccount.php
@@ -2,11 +2,11 @@
     <div class="row">
         <div class="col">
             <div class="userConnected d-flex flex-column mt-5">
-               <a class="exit" href="/controllers/homeCtrl.php"><img class="exitDoor" src="/public/assets/img/door.png" alt="porte ouverte icône de déconnexion"></a>
+               <a class="exit" href="/controllers/homeCtrl.php" title="Se déconnecter"><img class="exitDoor" src="/public/assets/img/door.png" alt="porte ouverte icône de déconnexion"></a>
                 <div class="text-center ">
                     <h1><span class="bdRenaissanceH1">B</span>ienvenue <?= $userConnected->pseudo ?> !</h1>
                 </div>
-                <div class="profilUser mt-3 p-3">
+                <div class="profilUser mt-5 p-3">
                     <div class="actionUser p-5">
                       <div class="btnUserAccount d-flex justify-content-around align-items-center"> <img src="/public/assets/img/plume.png" alt="icône édition"><a href="/controllers/userAccount/profilAccountCtrl.php">Mon <br> compte</a></div>
                       <div class="btnUserAccount d-flex justify-content-around align-items-center"><img src="/public/assets/img/icons8-panier-en-osier-96.png" alt=""><a href="/controllers/userAccount/ordersUserCtrl.php">Mes <br> achats</a></div>
