d6ab6ec40cb8c23a2a9bff7a21fd441ab5dd25dc
roxylya <adelinejeuniaux@gmail.com>
2023-03-27 17:03:02 +0200

    catalog et gallery ok front

---
+++ controllers/catalogMenCtrl.php
@@ -0,0 +1,40 @@
+<?php
+
+// on a besoin d'accéder à la db :
+require_once(__DIR__ . '/../config/Database.php');
+// on a besoin d'accéder aux constantes :
+require_once(__DIR__ . '/../config/constants.php');
+// on a besoin d'accéder au tableau de messages :
+require_once(__DIR__ . '/../config/config.php');
+// on a besoin d'accéder au helper :
+require_once(__DIR__ . '/../helper/dd.php');
+// on a besoin du model :
+require_once(__DIR__ . '/../models/User.php');
+
+
+try {
+    // si une session est en cours on la récupère :
+    session_start();
+    // on affiche l'header correspondant selon si user connecté ou non :
+    if (!isset($_SESSION['id_users'])) {
+        include(__DIR__ . '/../views/templates/header.php');
+    } else {
+        // on récupère l'id_users connecté:
+        $id_users = $_SESSION['id_users'];
+        // on récupère les informations de l'user connecté :
+        $userConnected = User::getById($id_users);
+        if ($userConnected->id_roles === 1 || $userConnected->id_roles === 2 || $userConnected->id_roles === 3) {
+            include(__DIR__ . '/../views/templates/headerUserAccount.php');
+        }
+    }
+} catch (\Throwable $th) {
+    // on récupère le message d'erreur :
+    $errorMessage = $th->getMessage();
+   
+    // on affiche la page d'erreur
+    include(__DIR__ . '/erreur.html');
+    die;
+}
+
+include(__DIR__ . '/../views/catalogMen.php');
+include(__DIR__ . '/../views/templates/footer.php');
---
+++ views/galleryPortraits.php
@@ -0,0 +1,26 @@
+<h1 class="text-center gold blackClover"><span class="bdRenaissanceH1">G</span>alerie</h1>
+<div class="w-100 d-flex navCatalog justify-content-center align-items-center  gold blackClover mt-3 py-3">
+    <a href="/galerie-portraits.html">Portraits</a> | 
+    <a href="/galerie-fresques.html">Fresques</a> | 
+    <a href="/galerie.html">Tout</a>
+</div>
+<div class="gallery d-flex flex-wrap justify-content-around align-items-center pt-2 mb-2">
+    <!-- article start -->
+    <div class="box col-lg-3 d-flex flex-column justify-content-center align-items-center p-3 m-md-3 my-3">
+        <!-- titre de l'article -->
+        <h2 class="blue blackClover">Cassiopée Nox</h2>
+        <p class="little gold fondamento"> Par Roxylya<span class="blue"> | </span>Le 12/12/2021<span class="blue"> | </span>Portraits.</p>
+        <!-- image de l'article -->
+        <img class="pt-4" src="/public/uploads/gallery/avatarCassiopeeNox08082022.jpg" alt="avatar Cassiopée Nox">
+    </div>
+    <!-- article end -->
+    <!-- article start -->
+    <div class="box col-lg-3 d-flex flex-column justify-content-center align-items-center p-3 m-md-3 my-3">
+        <!-- titre de l'article -->
+        <h2 class="blue blackClover">Cassiopée Nox</h2>
+        <p class="little gold fondamento"> Par Roxylya<span class="blue"> | </span>Le 12/12/2021<span class="blue"> | </span>Portraits.</p>
+        <!-- image de l'article -->
+        <img class="pt-4" src="/public/uploads/gallery/avatarCassiopeeNox08092021.jpg" alt="avatar Cassiopée Nox">
+    </div>
+    <!-- article end -->
+</div>
