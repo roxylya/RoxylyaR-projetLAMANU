dfec5c765de3ce7ccafa2b483629274cd4527f3b
roxylya <adelinejeuniaux@gmail.com>
2023-03-27 15:46:34 +0200

    catalog ok

--- views/gallery.php
+++ views/gallery.php
@@ -1,23 +1,52 @@
-<div class="text-center gold blackClover">
-    <h1><span class="bdRenaissanceH1">G</span>alerie</h1>
+<h1 class="text-center gold blackClover"><span class="bdRenaissanceH1">G</span>alerie</h1>
+<div class="w-100 d-flex navCatalog justify-content-center align-items-center  gold blackClover mt-3 py-3">
+    <a href="/galerie.html?categorie=portraits">Portraits</a> | 
+    <a href="/galerie.html?categorie=fresques">Fresques</a> | 
+    <a href="/galerie.html">Tout</a>
 </div>
-
-<div class="pt-lg-5 pt-3">
-
-</div>
-
-<div class="pt-lg-5 pt-3">
-
-</div>
-
-<div class="pt-lg-5 pt-3">
-
-</div>
-
-<div class="pt-lg-5 pt-3">
-
-</div>
-
-<div class="pt-lg-5 pt-3">
+<div class="gallery d-flex flex-wrap justify-content-around align-items-center pt-2 mb-2">
+    <!-- article start -->
+    <div class="box col-lg-3 d-flex flex-column justify-content-center align-items-center p-3 m-md-3 my-3">
+        <!-- titre de l'article -->
+        <h2 class="blue blackClover">Cassiopée Nox</h2>
+        <p class="little gold fondamento"> Par Roxylya <span class="blue">|</span> Le 28/08/2021 <span class="blue">|</span> Fresques.</p>
+        <!-- image de l'article -->
+        <img class="pt-4" src="/public/uploads/gallery/cassiopeeJangleNox28082021.png" alt="fresque Cassiopée Nox">
+    </div>
+    <!-- article start -->
+    <div class="box col-lg-3 d-flex flex-column justify-content-center align-items-center p-3 m-md-3 my-3">
+        <!-- titre de l'article -->
+        <h2 class="blue blackClover">Cassiopée Nox</h2>
+        <p class="little gold fondamento"> Par Roxylya<span class="blue"> | </span>Le 12/12/2021<span class="blue"> | </span>Fresques.</p>
+        <!-- image de l'article -->
+        <img class="pt-4" src="/public/uploads/gallery/cassiopeeNox29082021.png" alt="fresque Cassiopée Nox">
+    </div>
+    <!-- article start -->
+    <div class="box col-lg-3 d-flex flex-column justify-content-center align-items-center p-3 m-md-3 my-3">
+        <!-- titre de l'article -->
+        <h2 class="blue blackClover">Cassiopée Nox</h2>
+        <p class="little gold fondamento"> Par Roxylya<span class="blue"> | </span>Le 12/12/2021<span class="blue"> | </span>Fresques.</p>
+        <!-- image de l'article -->
+        <img class="pt-4" src="/public/uploads/gallery/cassiopeeJangleNox08092021.png" alt="fresque Cassiopée Nox">
+    </div>
+    <!-- article end -->
+     <!-- article start -->
+     <div class="box col-lg-3 d-flex flex-column justify-content-center align-items-center p-3 m-md-3 my-3">
+        <!-- titre de l'article -->
+        <h2 class="blue blackClover">Cassiopée Nox</h2>
+        <p class="little gold fondamento"> Par Roxylya<span class="blue"> | </span>Le 12/12/2021<span class="blue"> | </span>Fresques.</p>
+        <!-- image de l'article -->
+        <img class="pt-4" src="/public/uploads/gallery/cassiopeeJangleNox03102021.jpg" alt="fresque Cassiopée Nox">
+    </div>
+    <!-- article end -->
+     <!-- article start -->
+     <div class="box col-lg-3 d-flex flex-column justify-content-center align-items-center p-3 m-md-3 my-3">
+        <!-- titre de l'article -->
+        <h2 class="blue blackClover">Cassiopée Nox</h2>
+        <p class="little gold fondamento"> Par Roxylya<span class="blue"> | </span>Le 12/12/2021<span class="blue"> | </span>Fresques.</p>
+        <!-- image de l'article -->
+        <img class="pt-4" src="/public/uploads/gallery/cassiopeNox06042022.png" alt="fresque Cassiopée Nox">
+    </div>
+    <!-- article end -->
 
 </div>
